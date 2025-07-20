81b58943e58267f5436f2fd86f6d0fef7afecc39
houhongxu <hongxu.hou@goodwe.com>
2025-07-20 16:37:47 +0800

    feat: 添加nextjs-pages例子

---
+++ apps/nextjs-pages/src/pages/csr.js
@@ -0,0 +1,31 @@
+import { useEffect, useState } from 'react'
+
+export default function Page() {
+  const [count, setCount] = useState(null)
+  const [user, setUser] = useState(null)
+
+  useEffect(() => {
+    fetch(`http://localhost:3001/api/users/${Math.floor(Math.random() * 100)}`)
+      .then((res) => res.json())
+      .then((user) => {
+        setUser(user)
+        setCount(user.age)
+      })
+  }, [])
+
+  return (
+    <>
+      {user ? (
+        <div>
+          <h1>
+            {user.username} Counters {count} times
+          </h1>
+
+          <button onClick={() => setCount(count + 1)}>Click me</button>
+        </div>
+      ) : (
+        <div>Loading...</div>
+      )}
+    </>
+  )
+}
---
+++ apps/nextjs-pages/src/pages/ssg.js
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+
+export async function getStaticProps() {
+  const res = await fetch(
+    `http://localhost:3001/api/users/${Math.floor(Math.random() * 100)}`,
+  )
+
+  const user = await res.json()
+
+  return { props: { user } }
+}
+
+export default function Page({ user }) {
+  const [count, setCount] = useState(user.age)
+
+  return (
+    <div>
+      <h1>
+        {user.username} Counters {count} times
+      </h1>
+
+      <button onClick={() => setCount(count + 1)}>Click me</button>
+    </div>
+  )
+}
